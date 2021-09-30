f88c1627d901495851a412e65e0e9c7a56fe67a5
3jung <hsj7630@daum.net>
2021-09-30 22:26:51 +0900

    2021-09-30 세 수의 합

---
+++ 파이썬 알고리즘 인터뷰/07_배열/09_세 수의 합/브루트 포스로 계산.py
@@ -0,0 +1,26 @@
+# 브루트 포스로 계산 (15. 3Sum)
+
+import sys
+sys.stdin = open('input.txt')
+
+nums = list(map(int, input().split()))
+
+nums.sort()
+
+results = []
+
+# 브루트 포스 n^3 반복
+for i in range(len(nums) - 2):
+    # 중복된 값 건너뛰기
+    if i > 0 and nums[i] == nums[i -1]:
+        continue
+    for j in range(i + 1, len(nums) - 1):
+        if j > i + 1 and nums[j] == nums[j - 1]:
+            continue
+        for k in range(j + 1, len(nums)):
+            if k > j + 1 and nums[k] == nums[k - 1]:
+                continue
+            if nums[i] + nums[j] + nums[k] == 0:
+                results.append([nums[i], nums[j], nums[k]])
+
+print(results)
