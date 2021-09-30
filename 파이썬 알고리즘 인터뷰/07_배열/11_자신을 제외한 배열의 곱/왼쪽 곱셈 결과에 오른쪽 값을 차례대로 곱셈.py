57c576366e009b919620a7070e7b953fa02a180c
3jung <hsj7630@daum.net>
2021-10-01 00:53:26 +0900

    2021-10-01 자신을 제외한 배열의 곱

---
+++ 파이썬 알고리즘 인터뷰/07_배열/11_자신을 제외한 배열의 곱/왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈.py
@@ -0,0 +1,20 @@
+# 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈 (238. Product of Array Except Self)
+
+import sys
+sys.stdin = open('input.txt')
+
+nums = list(map(int, input().split()))
+
+out = []
+p = 1
+# 왼쪽 곱셈
+for i in range(0, len(nums)):
+    out.append(p)
+    p = p * nums[i]
+p = 1
+# 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+for i in range(len(nums) - 1, 0 - 1, -1):
+    out[i] = out[i] * p
+    p = p * nums[i]
+
+print(out)
