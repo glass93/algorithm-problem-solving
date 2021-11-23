213269c93877e95822ea491ab4c2d9857a8e9b8f
한상정 <hsj7630@daum.net>
2021-11-23 18:36:54 +0900

    211123 구현

---
+++ 이것이 코딩테스트/04_구현/02_시각/s1.py
@@ -0,0 +1,17 @@
+# 완전 탐색
+
+import sys
+sys.stdin = open('input.txt')
+
+# H를 입력받기
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
